facabae94c519401fdaabb8593e82de217e8106c
Uncle88 <andreyproskura88@gmail.com>
2017-08-26 20:49:17 +0300

    created new file - DoulbyLinkedList<T>

---
+++ DoublyLinkedList/DoublyLinkedList/DoulbyLinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DoublyLinkedList
+{
+    public class DoulbyLinkedList<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

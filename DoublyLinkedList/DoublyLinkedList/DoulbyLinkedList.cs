06ad1853aae9cfc13264f0ac261c7cdf75e37a5d
Uncle88 <andreyproskura88@gmail.com>
2017-08-26 21:20:29 +0300

    implement Add method for DoublyLinkedList<T>

--- DoublyLinkedList/DoublyLinkedList/DoulbyLinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList/DoulbyLinkedList.cs
@@ -6,6 +6,24 @@ namespace DoublyLinkedList
 {
     public class DoulbyLinkedList<T> : IEnumerable<T>
     {
+        public DoulbyUnit<T> head;
+        public DoulbyUnit<T> tail;
+        int count;
+
+        public void Add(T data)
+        {
+            DoulbyUnit<T> _doulbyUnit = new DoulbyUnit<T>(data);
+            if (head == null)
+               head = _doulbyUnit;
+            else
+            {
+                tail.Next = _doulbyUnit;
+                tail.Previous = tail;
+            }
+            tail = _doulbyUnit;
+            count++;
+        }
+ 
         public IEnumerator<T> GetEnumerator()
         {
             throw new NotImplementedException();
